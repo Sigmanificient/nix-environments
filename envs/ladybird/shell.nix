5a46e047db6105cbe4109fea6e25773effb49159
Sigmanificient <edhyjox@gmail.com>
2026-04-07 14:16:32 +0200

    ladybird: add env prefix to NIX_LDFLAGS

--- envs/ladybird/shell.nix
+++ envs/ladybird/shell.nix
@@ -24,5 +24,5 @@ pkgs.mkShell {
   ] ++ extraPkgs;
 
   # https://github.com/NixOS/nixpkgs/blob/79a8a723b9/pkgs/by-name/la/ladybird/package.nix#L144-L147
-  NIX_LDFLAGS = "-lGL";
+  env.NIX_LDFLAGS = "-lGL";
 }
